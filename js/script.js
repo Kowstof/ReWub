f75f8c4e6d3c1fc246d8c5f8b01174f3655a650f
Krystof pavlis <krystofpavlis@gmail.com>
2018-05-18 22:46:10 +1000

    broken button

--- js/script.js
+++ js/script.js
@@ -3,3 +3,14 @@
     waveColor: '#8ec7ff',
     progressColor: 'dodgerblue'
 });*/
+
+$(document).ready(function(){
+  $('.play-icon').click(function(){
+    if ($(this).text() == "play-arrow") {
+      $(this).text() = "pause";
+    }
+    else {
+      $(this).text() = "play-arrow";
+    }
+  });
+});
